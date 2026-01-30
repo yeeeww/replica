9144190a00bc4182e7cde3d5c6f31ffab592c484
yeeeww <yeangsshi@ewhain.net>
2026-01-30 21:16:58 +0900

    최종

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -14,17 +14,16 @@ const Footer = () => {
             </div>
             
             <div className="footer-kakao">
-              <a 
-                href="https://pf.kakao.com/" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="kakao-link"
-              >
+              <div className="kakao-link">
                 <svg className="kakao-icon" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M12 3C6.477 3 2 6.463 2 10.714c0 2.677 1.782 5.03 4.469 6.37-.17.614-.61 2.223-.7 2.566-.11.422.156.416.328.303.135-.09 2.15-1.46 3.023-2.052.602.088 1.222.134 1.88.134 5.523 0 10-3.463 10-7.321S17.523 3 12 3z"/>
                 </svg>
-                <span>카카오 고객센터 : ID(차후수정)</span>
-              </a>
+                <span>카카오 고객센터 : wiznoble</span>
+              </div>
+            </div>
+
+            <div className="footer-account">
+              <p>계좌번호 : im뱅크 262-13-084376</p>
             </div>
           </div>
         </div>
